ebeffa3d8dff806a793435b06c07bb3a76b23acc
Kurniawan Prasetyo <prassaiyan@gmail.com>
2017-07-31 17:04:16 +0700

    Fix template

--- resources/views/themes/eventually/html/footer.blade.php
+++ resources/views/themes/eventually/html/footer.blade.php
@@ -1,17 +1,17 @@
 <footer id="footer">
   <ul class="icons">
-    <li><a href="http://moesubs.com/" class="icon fa-globe"><span class="label">Website</span></a></li>
-    <li><a href="https://github.com/prasudiro/kirino-sexy" class="icon fa-github"><span class="label">Github</span></a></li>
-    <li><a href="https://www.facebook.com/Moesubs" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-    <li><a href="https://twitter.com/moesubs" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-    <li><a href="https://steamcommunity.com/id/prasudiro" class="icon fa-steam"><span class="label">Steam</span></a></li>
-    <li><a href="http://oreimo.wikia.com/wiki/Kirino_Kousaka" class="icon fa-wikipedia-w"><span class="label">Wikia</span></a></li>
-    <li><a href="https://www.reddit.com/r/Oreimo/" class="icon fa-reddit"><span class="label">Reddit</span></a></li>
-    <li><a href="https://www.tumblr.com/tagged/kirino" class="icon fa-tumblr"><span class="label">Tumblr</span></a></li>
-    <li><a href="https://www.instagram.com/kirino.kousaka/" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-    <li><a href="https://danbooru.donmai.us/posts?tags=kousaka_kirino" class="icon fa-image"><span class="label">Danbooru</span></a></li>
-    <li><a href="http://kbagi.com/Himeko" class="icon fa-video-camera"><span class="label">Kumpul Bagi</span></a></li>
-    <li><a href="mailto:admin@kirino.sexy" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+    <li><a target="_blank" href="http://moesubs.com/" class="icon fa-globe"><span class="label">Website</span></a></li>
+    <li><a target="_blank" href="https://github.com/prasudiro/kirino-sexy" class="icon fa-github"><span class="label">Github</span></a></li>
+    <li><a target="_blank" href="https://www.facebook.com/Moesubs" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+    <li><a target="_blank" href="https://twitter.com/moesubs" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+    <li><a target="_blank" href="https://steamcommunity.com/id/prasudiro" class="icon fa-steam"><span class="label">Steam</span></a></li>
+    <li><a target="_blank" href="http://oreimo.wikia.com/wiki/Kirino_Kousaka" class="icon fa-wikipedia-w"><span class="label">Wikia</span></a></li>
+    <li><a target="_blank" href="https://www.reddit.com/r/Oreimo/" class="icon fa-reddit"><span class="label">Reddit</span></a></li>
+    <li><a target="_blank" href="https://www.tumblr.com/tagged/kirino" class="icon fa-tumblr"><span class="label">Tumblr</span></a></li>
+    <li><a target="_blank" href="https://www.instagram.com/kirino.kousaka/" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+    <li><a target="_blank" href="https://danbooru.donmai.us/posts?tags=kousaka_kirino" class="icon fa-image"><span class="label">Danbooru</span></a></li>
+    <li><a target="_blank" href="http://kbagi.com/Himeko" class="icon fa-video-camera"><span class="label">Kumpul Bagi</span></a></li>
+    <li><a target="_blank" href="mailto:admin@kirino.sexy" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
     <li></li>
   </ul>
   <ul class="copyright">
